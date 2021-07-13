d2c95b6036c335e1386ced86a89739327c90ec2b
stevenacz <scoailazaa@gmail.com>
2021-07-13 18:03:30 -0500

    list component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
 import { HeroesComponent } from './heroes/heroe/heroes.component';
+import { ListComponent } from './heroes/list/list.component';
 
 @NgModule({
-	declarations: [AppComponent, CounterComponent, HeroesComponent],
+	declarations: [AppComponent, CounterComponent, HeroesComponent, ListComponent],
 	imports: [BrowserModule],
 	providers: [],
 	bootstrap: [AppComponent],
